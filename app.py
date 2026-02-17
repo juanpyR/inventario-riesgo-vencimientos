fd47b1da6e956bfa47b98355aedd32e8dd95d525
juanpyR <80225854+juanpyR@users.noreply.github.com>
2026-02-17 13:46:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -440,8 +440,8 @@ def mostrar_plan_accion(df_riesgo, fecha_hoy):
     if len(productos_vencidos) > 0:
         col1, col2, col3 = st.columns(3)
         with col1:
-            st.metric("Productos", len(productos_vencidos))
-            st.write(len(productos_vencidos))
+            st.metric("Productos", f"len(productos_vencidos) U")
+            
         with col2:
             st.metric("Unidades", int(productos_vencidos['Stock_Inicial'].sum()))
         with col3:
